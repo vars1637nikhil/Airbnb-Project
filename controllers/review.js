0b8984d12bb3908c3099661ab87b60c3b4680bbe
vars1637nikhil <nikhilvarshney205@gmail.com>
2026-06-22 16:58:56 +0530

    Major project ready for deployment

--- controllers/review.js
+++ controllers/review.js
@@ -2,9 +2,8 @@ const Listing = require("../models/listing.js");
 const Review = require("../models/review.js");
 
 module.exports.createReview = async (req, res) => {
-    console.log(req.params.id);
+  console.log(req.params.id);
   let listing = await Listing.findById(req.params.id);
-  // console.log("dwdwdq");
   let newReview = new Review(req.body.review);
   newReview.author = req.user._id;
   await newReview.save();
